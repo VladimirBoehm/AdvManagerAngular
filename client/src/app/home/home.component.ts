023f12420b0f4ec00103e086a13e56d7e60feaec
Vladimir Böhm <beym12345@gmail.com>
2024-09-30 17:35:02 +0200

    132

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
