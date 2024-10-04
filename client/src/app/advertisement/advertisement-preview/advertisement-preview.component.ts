fc37fa56c0c4c0b6d2be3240231dd45725d1412a
Vladimir Böhm <beym12345@gmail.com>
2024-10-04 12:22:35 +0200

    123

--- client/src/app/advertisement/advertisement-preview/advertisement-preview.component.ts
+++ client/src/app/advertisement/advertisement-preview/advertisement-preview.component.ts
@@ -1,6 +1,7 @@
 import { NgIf } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, inject, input, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-advertisement-preview',
@@ -10,9 +11,17 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './advertisement-preview.component.scss',
 })
 export class AdvertisementPreviewComponent {
+  @ViewChild('imageShowTemplate') imageShowTemplate?: any;
+  private modalService = inject(BsModalService);
   title = input.required<string | undefined >();
   message = input.required<string | undefined >();
   url = input.required<string | undefined >();
   linkName = input.required<string | undefined >();
   linkValue = input.required<string | undefined>();
+  modalRef?: BsModalRef;
+
+
+  showImage() {
+    this.modalRef = this.modalService.show(this.imageShowTemplate);
+  }
 }
