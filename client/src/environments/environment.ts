080f627176f96743a8e15dbe07ea193adb1bb59d
Vladimir Böhm <beym12345@gmail.com>
2024-10-05 16:11:48 +0200

    1213123

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://localhost:7117/',
-  apiUrl: 'https://8nb5wtf1-7117.euw.devtunnels.ms/',
+  apiUrl: 'https://fmrw76dr-7117.euw.devtunnels.ms/',
 };
