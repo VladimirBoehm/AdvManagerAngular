3691921b7d76bc6d03030020f739803e440660db
Vladimir Böhm <beym12345@gmail.com>
2024-10-23 13:37:08 +0200

    123

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -1,11 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { catchError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
   const toastr = inject(ToastrService);
 
   return next(req).pipe(
@@ -26,14 +24,16 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
             }
             break;
           case 401:
-            toastr.error('Unauthorised', error.status)
+            toastr.error('Unauthorized', error.status)
             break;
           case 404:
-            router.navigateByUrl('/not-found');
+           // router.navigateByUrl('/not-found');
+            toastr.error('Page not found')
             break;
           case 500:
-            const navigationExtras: NavigationExtras = {state: {error: error.error}};
-            router.navigateByUrl('/server-error', navigationExtras);
+           // const navigationExtras: NavigationExtras = {state: {error: error.error}};
+            //router.navigateByUrl('/server-error', navigationExtras);
+            toastr.error('server-error', error.status)
             break;
           default:
             toastr.error('Something unexpected went wrong');
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,22 +4,33 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { telegramInitDataInterceptor } from './_interceptors/telegram-init-data.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { loadingInterceptor } from './_interceptors/loading.interceptor';
+import { errorInterceptor } from './_interceptors/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([telegramInitDataInterceptor, loadingInterceptor])
+      withInterceptors([
+        errorInterceptor,
+        telegramInitDataInterceptor,
+        loadingInterceptor,
+      ])
     ),
     provideAnimationsAsync(),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top-full-width',
+    }),
     importProvidersFrom(ModalModule.forRoot()),
   ],
 };
