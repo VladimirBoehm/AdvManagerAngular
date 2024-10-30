d235ae327eb0e95305af52ce514e4ab638c0ee13
Vladimir Böhm <beym12345@gmail.com>
2024-10-30 13:50:21 +0100

    123

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { NgIf } from '@angular/common';
 import { AdvListType } from '../_framework/constants/advListType';
 import { SharedModule } from '../_framework/modules/sharedModule';
 import { BusyService } from '../_services/busy.service';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,8 @@ export class HomeComponent implements OnInit {
   advListType = AdvListType;
   advertisementService = inject(AdvertisementService);
   busyService = inject(BusyService);
+  accountService = inject(AccountService);
+
 
   advertisementsToValidateCount: number = 0;
 
