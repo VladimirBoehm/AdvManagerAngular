a7b5740cbb6199fb33e49933f7aaba3f182727a8
Vladimir Böhm <beym12345@gmail.com>
2024-10-31 20:26:04 +0100

    132

--- client/src/app/_framework/component/mat-confirm-modal/mat-confirm-modal.component.ts
+++ client/src/app/_framework/component/mat-confirm-modal/mat-confirm-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  inject,
+  Inject,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogRef,
@@ -9,6 +16,7 @@ import {
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { ConfirmDialogData } from '../../../_models/confirm-dialog-data';
+import { TelegramBackButtonService } from '../../telegramBackButtonService';
 
 @Component({
   selector: 'app-mat-confirm-modal',
@@ -24,12 +32,21 @@ import { ConfirmDialogData } from '../../../_models/confirm-dialog-data';
   styleUrl: './mat-confirm-modal.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class MatConfirmModalComponent {
+export class MatConfirmModalComponent implements OnInit, OnDestroy {
+  private backButtonService = inject(TelegramBackButtonService);
   constructor(
     public dialogRef: MatDialogRef<MatConfirmModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData
   ) {}
+  
+  ngOnInit(): void {
+    this.backButtonService.setCloseDialogHandler(() => this.close());
+  }
 
+  close(): void {
+    this.dialogRef.close();
+  }
+  ngOnDestroy(): void {
+    this.backButtonService.removeCloseDialogHandler();
+  }
 }
-
-
