b656cbeb86ee38e249214b59c8c96ef8315df282
Vladimir Böhm <beym12345@gmail.com>
2024-11-05 13:10:36 +0100

    123

--- client/src/app/_framework/component/skeleton-full-screen/skeleton-full-screen.component.ts
+++ client/src/app/_framework/component/skeleton-full-screen/skeleton-full-screen.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SharedModule } from '../../modules/sharedModule';
 
 @Component({
   selector: 'app-skeleton-full-screen',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './skeleton-full-screen.component.html',
-  styleUrl: './skeleton-full-screen.component.scss'
+  styleUrl: './skeleton-full-screen.component.scss',
 })
 export class SkeletonFullScreenComponent {
-
+  @Input() shouldShowHeaderPanel?: boolean = true;
 }
