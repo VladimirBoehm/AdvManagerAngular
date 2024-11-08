5e4d7264814ea4f8ba879cda4e0f386020959486
Vladimir Böhm <beym12345@gmail.com>
2024-11-08 10:45:41 +0100

    init commit

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,7 +6,7 @@ function determineApiUrl(): string {
   const hostname = window.location.hostname;
 
   if (hostname === 'gray-coast-0eea7ee03.5.azurestaticapps.net') {
-    return 'https://production-api-url.com/';
+    return 'ЕУЫЫЫ   https://production-api-url.com/';
   } else {
     return 'https://ljscvw91-7117.euw.devtunnels.ms/';
   }
