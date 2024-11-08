680527d8303ef73315cb3d28d8c29badf2d1f9ed
Vladimir Böhm <beym12345@gmail.com>
2024-11-08 10:38:53 +0100

    environment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://ljscvw91-7117.euw.devtunnels.ms/',
+  apiUrl: determineApiUrl(),
 };
+
+function determineApiUrl(): string {
+  const hostname = window.location.hostname;
+
+  if (hostname === 'gray-coast-0eea7ee03.5.azurestaticapps.net') {
+    return 'https://production-api-url.com/';
+  } else {
+    return 'https://ljscvw91-7117.euw.devtunnels.ms/';
+  }
+}
