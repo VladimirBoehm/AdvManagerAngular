fed7ebac05018fca07e78fcde5733a661848a764
Vladimir Böhm <beym12345@gmail.com>
2024-11-13 12:59:34 +0100

    123

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
 
 function determineApiUrl(): string {
   const hostname = window.location.hostname;
-  const developUrl = 'https://631h1jhw-7117.euw.devtunnels.ms/';
+  const developUrl = 'https://1dqj3px2-7117.euw.devtunnels.ms/';
 
   if (hostname === 'gray-coast-0eea7ee03.5.azurestaticapps.net') {
     return 'https://chatcontrolwebapi.azurewebsites.net/';
