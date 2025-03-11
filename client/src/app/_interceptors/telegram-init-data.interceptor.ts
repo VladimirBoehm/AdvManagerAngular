ecdb7d0d84c768f29cb1427df140948d5b6cc43f
Vladimir Böhm <beym12345@gmail.com>
2025-03-11 14:44:58 +0100

    Add handling for OPTIONS requests in Telegram interceptor

--- client/src/app/_interceptors/telegram-init-data.interceptor.ts
+++ client/src/app/_interceptors/telegram-init-data.interceptor.ts
@@ -8,6 +8,10 @@ declare global {
 }
 
 export const telegramInitDataInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.method === 'OPTIONS') {
+    return next(req);
+  }
+
   const initData = window.Telegram?.WebApp?.initData;
   const jwtToken = localStorage.getItem(LOCAL_STORAGE_CONSTANTS.JWT_TOKEN);
 
@@ -15,7 +19,7 @@ export const telegramInitDataInterceptor: HttpInterceptorFn = (req, next) => {
     const clonedRequest = req.clone({
       setHeaders: {
         initData: initData,
-         Authorization: `Bearer ${jwtToken}`,
+        Authorization: `Bearer ${jwtToken}`,
       },
     });
     return next(clonedRequest);
