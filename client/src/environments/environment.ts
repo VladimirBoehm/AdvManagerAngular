9c8d2dcf1030c66a74a649971ce4dcd639719bc2
Vladimir Böhm <beym12345@gmail.com>
2025-03-13 12:50:38 +0100

    Update test API URL in environment configuration

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 const prodHostname = 'gray-coast-0eea7ee03.5.azurestaticapps.net';
 const prodApi = 'https://chatcontrolwebapi.azurewebsites.net/';
 //----------------------------------------------------------------
-const testApi = 'https://k9lqv9jc-7117.euw.devtunnels.ms/';
+const testApi = 'https://snmsgzgw-7117.euw.devtunnels.ms/';
 
 function determineApiUrl(): string {
   const hostname = window.location.hostname;
